501d2e3382582e97af04ac3ff7779b473ed6bafc
Jae Ro <jaegro93@gmail.com>
2023-01-21 23:27:10 -0500

    Created schema.sql file based on csv data for people, places

---
+++ schema.sql
@@ -0,0 +1,21 @@
+drop table if exists people;
+drop table if exists places;
+
+-- Assuming non pk fields can be Null
+create table `people` (
+  `id` int not null auto_increment,
+  `Given_name` varchar(80) default null,
+  `Family_name` varchar(80) default null,
+  `Date_of_birth` DATE default null,
+  `Place_of_birth` varchar(80) default null, -- represents to City of Birth
+  primary key (`id`)
+);
+
+-- Assuming non pk fields can be Null
+create table `places` (
+  `id` int not null auto_increment,
+  `City` varchar(80) default null,
+  `County` varchar(80) default null,
+  `Country` varchar(80) default null,
+  primary key (`id`)
+);
