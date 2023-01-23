13bf65be47b25105e4cfa85dcf00b3ce97441794
Jae Ro <jaegro93@gmail.com>
2023-01-23 00:49:38 -0500

    Updated comments in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,17 +1,18 @@
+-- Drop tables if already exist. This script is run at database startup
 drop table if exists people;
 drop table if exists places;
 
--- Assuming non pk fields can be Null
+-- Assuming non-pk fields can be Null
 create table `people` (
   `id` int not null auto_increment,
   `Given_name` varchar(80) default null,
   `Family_name` varchar(80) default null,
   `Date_of_birth` DATE default null,
-  `Place_of_birth` varchar(80) default null, -- represents to City of Birth
+  `Place_of_birth` varchar(80) default null, -- represents the City of Birth
   primary key (`id`)
 );
 
--- Assuming non pk fields can be Null
+-- Assuming non-pk fields can be Null
 create table `places` (
   `id` int not null auto_increment,
   `City` varchar(80) default null,
